279b2693fd47a61536fc1c77b2cc382435442ff1
Clinton <unegbuclinton5@gmail.com>
2023-09-23 00:03:25 +0100

    pipeline

--- get-link/src/components/molecules/rules/index.tsx
+++ get-link/src/components/molecules/rules/index.tsx
@@ -9,11 +9,11 @@ const Rules = () => {
         </h3>
         <p className='text-[13px] lg:text-sm'>
           Our tech hackathon is a melting pot of visionaries, and its purpose is
-          as clear as day: to shape the future. Whether you're a coding genius,
-          a design maverick, or a concept wizard, you'll have the chance to
-          transform your ideas into reality. Solving real-world problems,
-          pushing the boundaries of technology, and creating solutions that can
-          change the world, that's what we're all about!
+          as clear as day: to shape the future. Whether you&apos;re a coding
+          genius, a design maverick, or a concept wizard, you&apos;ll have the
+          chance to transform your ideas into reality. Solving real-world
+          problems, pushing the boundaries of technology, and creating solutions
+          that can change the world, that&apos;s what we&apos;re all about!
         </p>
       </div>
       <img src='/rules.svg' alt='' />
